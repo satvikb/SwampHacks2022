63ad8bcf188b1dadd9a56c45ebfa934edb2b11a1
Satvik Borra <borrasatvik@gmail.com>
2022-01-28 23:59:25 -0500

    button id change

--- extension/popup.js
+++ extension/popup.js
@@ -1,12 +1,12 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+let startTranslationButton = document.getElementById("startTranslationButton");
 
 chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+  startTranslationButton.style.backgroundColor = color;
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+startTranslationButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
