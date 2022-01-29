51dd3b7fa1ed66ee6a60244c200aa4e60f314755
Satvik Borra <borrasatvik@gmail.com>
2022-01-29 18:53:56 -0500

    add basic translate

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ var bodyParser = require('body-parser')
 
 const app = express()
 const port = 8000
-const language = require('@google-cloud/language');
-const client = new language.LanguageServiceClient();
+const Language = require('@google-cloud/language');
+const {Translate} = require('@google-cloud/translate').v2;
+
+const language = new Language.LanguageServiceClient();
+const translate = new Translate();
+
 var jsonParser = bodyParser.json()
 
 app.use(bodyParser.json({limit: "50mb"}));
@@ -19,15 +23,27 @@ app.post('/getSentences', jsonParser, async function(request,response){
   // Detects the sentiment of the text
   try{
     // const [result] = await client.analyzeSentiment({document: document});
-    const [result] = await client.analyzeSyntax({document: document});
+    const [result] = await language.analyzeSyntax({document: document});
 
-    const sentiment = result.sentences;
-  
+    const sentencesObject = result.sentences;
     
+    var sentences = [];
+    for (var i = 0; i < sentencesObject.length; i++) {
+      var sentence = sentencesObject[i];
+      var sentenceText = sentence.text.content;
+      // TODO filter out sentences that are too short, etc
+      sentences.push(sentenceText);
+    }
+    
+    var target = "sr-Latn"
+    let [translations] = await translate.translate(sentences, target);
+    translations = Array.isArray(translations) ? translations : [translations];
+
+ 
     // console.log(`Text: ${text}`);
     // console.log(`Sentiment score: ${sentiment.score}`);
     // console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
-    response.send(sentiment)
+    response.send(translations)
   }catch(err){
     console.log(err)
     response.send(err)
